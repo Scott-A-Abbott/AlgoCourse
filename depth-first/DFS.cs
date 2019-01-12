925ad58e46755d0c5242ed5ff284b69b56c06670
Scott Abbott <scott.abbott.a@gmail.com>
2019-01-11 23:11:46 -0700

    finished dfs

--- depth-first/DFS.cs
+++ depth-first/DFS.cs
@@ -2,19 +2,36 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-public class DFS{
+public class DFS
+{
 
-    public static void dfs(List<Vertex> vertices){
+
+
+    public static void dfs(List<Vertex> vertices)
+    {
         foreach (Vertex v in vertices)
         {
-            if(!v.Visited)
+            if (!v.Visited)
             {
                 v.Visited = true;
-                dfsWithStack(v);
+                // dfsWithStack(v);
+                dfsWithRecursion(v);
             }
         }
     }
+    private static void dfsWithRecursion(Vertex root)
+    {
+        Console.WriteLine(root.Name);
 
+        foreach (Vertex neighbor in root.NeighborList)
+        {
+            if (!neighbor.Visited)
+            {
+                neighbor.Visited = true;
+                dfsWithRecursion(neighbor);
+            }
+        }
+    }
     private static void dfsWithStack(Vertex root)
     {
         Stack<Vertex> stack = new Stack<Vertex>();
@@ -22,14 +39,14 @@ public class DFS{
         stack.Push(root);
         root.Visited = true;
 
-        while(stack.ToArray().Length > 0)
+        while (stack.ToArray().Length > 0)
         {
             Vertex actualVertex = stack.Pop();
             Console.WriteLine(actualVertex.Name);
 
             foreach (Vertex v in actualVertex.NeighborList)
             {
-                if(!v.Visited)
+                if (!v.Visited)
                 {
                     v.Visited = true;
                     stack.Push(v);
